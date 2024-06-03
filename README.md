0ef45748273fbfaaf8f26fa89248258d47f631c8
hilderonny <hilderonny2014@gmail.com>
2024-06-03 16:02:27 +0200

    Added test script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # taskworker-speak
+
+## Installation
+
+https://stackoverflow.com/questions/64261546/how-to-solve-error-microsoft-visual-c-14-0-or-greater-is-required-when-inst
+
+Model: C:\Users\XXXXXX\AppData\Local\tts\tts_models--multilingual--multi-dataset--xtts_v2
+
+Set model path: https://github.com/coqui-ai/TTS/discussions/2476
---
+++ test.py
@@ -0,0 +1,48 @@
+import time
+import os
+
+os.environ["TTS_HOME"] = "./data"
+
+ct = time.time()
+
+def timediff():
+    global ct
+    now = time.time()
+    diff = now - ct
+    ct = now
+    return str(diff) + "\n"
+
+# https://huggingface.co/coqui/XTTS-v2
+# https://pypi.org/project/coqui-tts/
+
+print(timediff(), "Importing Torch ...")
+import torch
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Device: ", device)
+print(timediff(), "Importing TTS ...")
+from TTS.api import TTS
+
+print(timediff(), "Initialize TTS ...")
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+# Run TTS
+# Text to speech to a file
+print(timediff(), "Generating en ...")
+tts.tts_to_file(
+    text="It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent.", 
+    speaker_wav="KITT.wav", 
+    language="en", 
+    file_path="output_en.wav"
+)
+# Needs 5 seconds, real time factor 0.64
+
+print(timediff(), "Generating de ...")
+tts.tts_to_file(
+    text="In Deggendorf in Niederbayern ist wegen des Hochwassers ein Passagierschiff evakuiert worden. Mehr als 140 Menschen würden seit den Mittagsstunden vom Schiff gebracht, sagte eine Sprecherin des Landratsamts am Montag. Wegen des Hochwassers an der Donau könne das Schiff nicht weiterreisen. Bei den Passagieren handle es sich überwiegend um ältere Menschen. Es gebe aber bislang keinen medizinischen Notfall an Bord, hieß es weiter. Boote waren im Einsatz, um die Menschen an Land zu bringen.", 
+    speaker_wav="KITT.wav", 
+    language="de", 
+    file_path="output_de.wav"
+)
+# Needs 28 seconds, real time factor 0.66
+
+print(timediff(), "DONE")
